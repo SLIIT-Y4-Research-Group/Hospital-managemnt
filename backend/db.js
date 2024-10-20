483960c01d64a4c29b20ed3375d21f7480fff833
Winsara A.J <it22150516@my.sliit.lk>
2024-10-20 12:47:29 +0530

    added singaton call to database

---
+++ backend/db.js
@@ -0,0 +1,33 @@
+// db.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+class Database {
+    constructor() {
+        if (!Database.instance) {
+            this.connect();
+            Database.instance = this;
+        }
+        return Database.instance;
+    }
+
+    async connect() {
+        const mongoDBURL = 'mongodb+srv://Dhanuka:20020502@mernapp.emyz11d.mongodb.net/?retryWrites=true&w=majority&appName=MERNApp'; // Use the environment variable
+        try {
+            // Remove deprecated options
+            await mongoose.connect(mongoDBURL);
+            console.log('App connected to database');
+        } catch (error) {
+            console.error('Database connection error:', error);
+            process.exit(1); // Exit if connection fails
+        }
+    }
+}
+
+// Create a single instance of the Database
+const instance = new Database();
+Object.freeze(instance); // Freeze the instance to prevent modification
+
+export default instance;
